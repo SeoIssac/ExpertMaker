c64e9083aba5774950af2023f09e4bd2c09393cb
SeoIssac <tjehdrms98@hs.ac.kr>
2023-05-23 18:04:07 +0900

    table_insertion.sql 수정

--- demo/src/main/DB/table_insertion.sql
+++ demo/src/main/DB/table_insertion.sql
@@ -9,10 +9,24 @@ CREATE TABLE if not exists Users
 CREATE TABLE if not exists Questions
 (
      ID INT PRIMARY KEY AUTO_INCREMENT,
+     Qnum INT NOT NULL,
+     Level VARCHAR(20) NOT NULL,
      Question VARCHAR(500) NOT NULL,
-     Usable_Functions VARCHAR(50) NOT NULL,
-     Hint VARCHAR(500) NOT NULL,
+     Usable_Functions VARCHAR(50) NOT NULL
+);
+CREATE TABLE if not exists Answers
+(
+     ID INT PRIMARY KEY AUTO_INCREMENT,
+     question_ID INT,
+     fOREIGN KEY(question_ID) REFERENCES Questions(ID) ON UPDATE CASCADE,
      Answer VARCHAR(500) NOT NULL
+);
+CREATE TABLE if not exists Hints
+(
+     ID INT PRIMARY KEY AUTO_INCREMENT,
+     question_ID INT,
+     fOREIGN KEY(question_ID) REFERENCES Questions(ID) ON UPDATE CASCADE,
+     Hint VARCHAR(500) NOT NULL
 );
 CREATE TABLE if not exists Functions
 (
@@ -27,7 +41,6 @@ CREATE TABLE if not exists Solve
      fOREIGN KEY(user_ID) REFERENCES Users(ID) ON UPDATE CASCADE,
      question_ID INT,
      fOREIGN KEY(question_ID) REFERENCES Questions(ID) ON UPDATE CASCADE,
-     Solving_Date DATETIME NOT NULL,
      Submitted_Answer VARCHAR(500),
      Correctness INT DEFAULT 0
 );
@@ -38,6 +51,5 @@ CREATE TABLE if not exists Refer
      fOREIGN KEY(user_ID) REFERENCES Users(ID) ON UPDATE CASCADE,
      function_ID INT,
      fOREIGN KEY(function_ID) REFERENCES Functions(ID) ON UPDATE CASCADE,
-     Refering_Date DATETIME NOT NULL,
      Search_Word VARCHAR(50)
 );
