900f9d90fed1ea9210e2b96ba01008a373e25fed
daramabasa <canbus052@naver.com>
2023-05-26 03:28:41 +0900

    프론트쪽 파일 이동 + SpringBoot 실행 시 화면 보이게 조정

--- demo/src/main/frontend/src/App.js
+++ demo/src/main/frontend/src/App.js
@@ -32,10 +32,7 @@ function App() {
 
     return (
         <>
-            <h3>문제 번호: {question.id}</h3>
-            <h3>문제 난이도: {question.level}</h3>
-            <h3>문제 내용: {question.quest}</h3>
-            <h3>문제 답: {question.answer}</h3>
+            <h1>test</h1>
         </>
     );
 }
