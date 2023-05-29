5a9ca74aeb34e103516baa78c6d99acf69b6abb7
daramabasa <canbus052@naver.com>
2023-05-30 08:53:21 +0900

    React로 화면 전환 & Spring 쪽에서 데이터 받기 진행 중

---
+++ demo/src/main/java/ExpertMaker/demo/controller/ReactPageController.java
@@ -0,0 +1,17 @@
+package ExpertMaker.demo.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping("/api/")
+public class ReactPageController {
+
+    @GetMapping("function_dic")
+    public String dictionary(){
+        return "Function_dic";
+    }
+}
