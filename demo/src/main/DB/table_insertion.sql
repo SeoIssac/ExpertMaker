757004497d52af64ebb183c720969eb85a64ff15
SeoIssac <tjehdrms98@hs.ac.kr>
2023-06-01 15:54:56 +0900

    sql 수정 작업

--- demo/src/main/DB/table_insertion.sql
+++ demo/src/main/DB/table_insertion.sql
@@ -1,19 +1,17 @@
-use expertname;
-CREATE TABLE if not exists Users
-(
-     ID INT PRIMARY KEY AUTO_INCREMENT,
-     IPaddress VARCHAR(20) NOT NULL,
-     Progression VARCHAR(20),
-     Score VARCHAR(20)
-);
+USE expertname;
+DROP TABLE Functions;
+DROP TABLE Answers;
+DROP TABLE Hints;
+DROP TABLE Questions;
 CREATE TABLE if not exists Questions
 (
      ID INT PRIMARY KEY AUTO_INCREMENT,
-     Qnum INT NOT NULL,
      Level VARCHAR(20) NOT NULL,
+     Qnum INT NOT NULL,
      Question VARCHAR(500) NOT NULL,
      Usable_Functions VARCHAR(50) NOT NULL
 );
+CREATE UNIQUE INDEX QNL on Questions(Level, Qnum);
 CREATE TABLE if not exists Answers
 (
      ID INT PRIMARY KEY AUTO_INCREMENT,
@@ -33,23 +31,4 @@ CREATE TABLE if not exists Functions
      ID INT PRIMARY KEY AUTO_INCREMENT,
      Function_Name VARCHAR(50) NOT NULL,
      Function_info VARCHAR(500) NOT NULL
-);
-CREATE TABLE if not exists Solve
-(
-     ID INT PRIMARY KEY AUTO_INCREMENT,
-     user_ID INT,
-     fOREIGN KEY(user_ID) REFERENCES Users(ID) ON UPDATE CASCADE,
-     question_ID INT,
-     fOREIGN KEY(question_ID) REFERENCES Questions(ID) ON UPDATE CASCADE,
-     Submitted_Answer VARCHAR(500),
-     Correctness INT DEFAULT 0
-);
-CREATE TABLE if not exists Refer
-(
-     ID INT PRIMARY KEY AUTO_INCREMENT,
-	 user_ID INT,
-     fOREIGN KEY(user_ID) REFERENCES Users(ID) ON UPDATE CASCADE,
-     function_ID INT,
-     fOREIGN KEY(function_ID) REFERENCES Functions(ID) ON UPDATE CASCADE,
-     Search_Word VARCHAR(50)
 );
